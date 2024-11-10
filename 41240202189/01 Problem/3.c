a4b3e0f4217780af7a9fe700aef69e92e95286a5
armanbin007 <160709760+armanbin007@users.noreply.github.com>
2024-11-10 23:26:47 +0600

    Delete 41240202189 directory

--- 41240202189/01 Problem/3.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("The question is - \"How to write a\\comment/ in C programming language?\"\n");
-    
-    return 0;
-}
--- 41240202189/02 Problem/2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float Pi = 3.14, r;
-    scanf("%f", &r);
-    float A = 2 * Pi * r; // A = Area
-    printf("Area: %.2f\n", A);
-    
-    return 0;
-}
